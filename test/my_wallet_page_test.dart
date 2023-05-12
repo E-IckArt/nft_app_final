44b94f204b28d4104c48db1273b21866b5d9031d
emilie <emi.ickart@gmail.com>
2023-05-12 14:21:58 +0200

    Add increment euro wallet test

--- test/my_wallet_page_test.dart
+++ test/my_wallet_page_test.dart
@@ -5,9 +5,18 @@ void main() {
   group('Test App', () {
     var myWallet = MyWallet();
 
+
+
     test('Test increment BTC wallet', () {
       myWallet.incrementBTCWallet();
       expect(myWallet.amountBtc, 1);
+    });
+
+    test('Test increment Euro wallet', () {
+      myWallet.amountBtc = 10.00;
+      myWallet.calculatedAmount = 2.00;
+      myWallet.incrementEuroWallet(1.00);
+      expect(myWallet.amountEuro, 2.00);
     });
   });
 }
